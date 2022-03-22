c616c3802ba95bf2377509ce85d82f27dcb2f7c2
Hocine <ocineh@hotmail.com>
2022-03-22 11:29:57 +0100

    Add the chiffre and unbounded_int structure

--- library.h
+++ library.h
@@ -1,6 +1,21 @@
 #ifndef PROJET_LIBRARY_H
 #define PROJET_LIBRARY_H
 
+#include <stddef.h>
+
 void hello(void);
+
+typedef struct chiffre {
+	struct chiffre *suivant;
+	char c;
+	struct chiffre *precedent;
+} chiffre;
+
+typedef struct {
+	char signe;
+	size_t len;
+	chiffre *premier;
+	chiffre *dernier;
+} unbounded_int;
 
 #endif //PROJET_LIBRARY_H
