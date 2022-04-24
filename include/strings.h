4eee5a89911014fb774baf3c9f8390eabcacb39a
Hocine <ocineh@hotmail.com>
2022-04-24 23:11:10 +0200

    Add a function to get the first occurrence of a char in a string

--- include/strings.h
+++ include/strings.h
@@ -22,4 +22,13 @@ extern char *strip(char *c);
  */
 extern size_t split(char *line, char separator, char ***tokens);
 
+/**
+ * Get the index of the first occurrence of the character in the string
+ *
+ * @param s a string
+ * @param c a character
+ * @return an index in the string or -1 if not found
+ */
+size_t index_of(const char *s, char c);
+
 #endif //PROJET_STRINGS_H
--- src/strings.c
+++ src/strings.c
@@ -43,3 +43,10 @@ size_t split(char *line, char separator, char ***tokens) {
 	}
 	return count;
 }
+
+size_t index_of(const char *s, char c) {
+	size_t len = strlen(s);
+	for(size_t i = 0; i < len; ++i)
+		if(s[i] == c) return i;
+	return -1;
+}
