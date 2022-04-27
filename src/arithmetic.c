5c00aba0d4f9ac239cc1d104e3a3219c7a4d5dff
Hocine <ocineh@hotmail.com>
2022-04-27 16:02:24 +0200

    Add functions to create nodes

--- src/arithmetic.c
+++ src/arithmetic.c
@@ -1,1 +1,18 @@
+#include <malloc.h>
 #include "arithmetic.h"
+
+node *operator_to_node(operator op, node *left, node *right) {
+	node *n = malloc(sizeof(node));
+	n->operator = op;
+	n->left = left;
+	n->right = right;
+	return n;
+}
+
+node *value_to_node(unbounded_int value) {
+	node *n = malloc(sizeof(node));
+	n->operator = NONE;
+	n->operand = value;
+	n->left = n->right = NULL;
+	return n;
+}
