872df9c323eca6408a37c5b0b8715a9246e32337
Hocine <ocineh@hotmail.com>
2022-05-05 16:10:28 +0200

    Change the tests to use random numbers

--- test/test_unbounded_int.c
+++ test/test_unbounded_int.c
@@ -1,10 +1,10 @@
-#include <string.h>
-#include <malloc.h>
+#include <stdlib.h>
 #include "test_unbounded_int.h"
 #include "unbounded_int.h"
 #include "strings.h"
 
 static bool uint_cmp_char(unbounded_int u, char *s) {
+	if(*s == '-') ++s;
 	size_t len = strlen(s);
 	if(len != u.len) return false;
 	chiffre *p = u.premier;
@@ -16,18 +16,39 @@ static bool uint_cmp_char(unbounded_int u, char *s) {
 	return true;
 }
 
-bool test_create_uint_from_string(void) {
-	unbounded_int u = string2unbounded_int("364");
-	bool result = !isNaN(u) && u.signe == '+' && uint_cmp_char(u, "364");
+static long long random_ll(long long min, long long max) {
+	return (min + (rand() % (max - min + 1)));
+}
+
+#define POSITIVE (random_ll(0, 10000))
+#define NEGATIVE (random_ll(-10000, 0))
+
+static size_t len(long long n) {
+	if(n == 0) return 1;
+	size_t len = 0;
+	while(n != 0)
+		n /= 10, ++len;
+	return len + (n < 0);
+}
+
+static bool test_create_uint_from_string(long long n) {
+	char *s = malloc(len(n) + 1);
+	sprintf(s, "%lld", n);
+	char signe = s[0] == '-' ? '-' : '+';
+
+	unbounded_int u = string2unbounded_int(s);
+	bool result = !isNaN(u) && u.signe == signe && uint_cmp_char(u, s);
 	free_unbounded_int(&u);
+	free(s);
 	return result;
 }
 
+bool test_create_uint_from_string_1(void) {
+	return test_create_uint_from_string(POSITIVE);
+}
+
 bool test_create_uint_from_string_2(void) {
-	unbounded_int u = string2unbounded_int("-924");
-	bool result = !isNaN(u) && u.signe == '-' && uint_cmp_char(u, "924");
-	free_unbounded_int(&u);
-	return result;
+	return test_create_uint_from_string(NEGATIVE);
 }
 
 bool test_create_uint_from_string_with_invalid_string(void) {
@@ -40,330 +61,266 @@ bool test_create_uint_from_string_with_invalid_string_2(void) {
 	return isNaN(u);
 }
 
-bool test_create_uint_from_ll(void) {
-	unbounded_int u = ll2unbounded_int(924);
-	bool result = !isNaN(u) && u.signe == '+' && uint_cmp_char(u, "924");
+bool test_create_uint_from_ll_1(void) {
+	long long n = POSITIVE;
+	char *s = malloc(len(n) + 1);
+	sprintf(s, "%lld", n);
+
+	unbounded_int u = ll2unbounded_int(n);
+	bool result = !isNaN(u) && u.signe == '+' && uint_cmp_char(u, s);
 	free_unbounded_int(&u);
+	free(s);
 	return result;
 }
 
 bool test_create_uint_from_ll_2(void) {
-	unbounded_int u = ll2unbounded_int(-12);
-	bool result = !isNaN(u) && u.signe == '-' && uint_cmp_char(u, "12");
-	free_unbounded_int(&u);
-	return result;
-}
+	long long n = NEGATIVE;
+	char *s = malloc(len(n) + 1);
+	sprintf(s, "%lld", n);
 
-bool test_create_string_from_uint(void) {
-	unbounded_int u = string2unbounded_int("924");
-	char *s = unbounded_int2string(u);
-	bool result = strcmp(s, "924") == 0;
+	unbounded_int u = ll2unbounded_int(n);
+	bool result = !isNaN(u) && u.signe == '-' && uint_cmp_char(u, s);
 	free_unbounded_int(&u);
 	free(s);
 	return result;
+}
+
+static bool test_create_string_from_uint(long long n) {
+	char *s = malloc(len(n) + 1);
+	sprintf(s, "%lld", n);
+
+	unbounded_int u = ll2unbounded_int(n);
+	bool result = !isNaN(u) && u.signe == ((n < 0) ? '-' : '+') && uint_cmp_char(u, s);
+	free_unbounded_int(&u);
+	free(s);
+	return result;
+}
+
+bool test_create_string_from_uint_1(void) {
+	for(int i = 0; i < 100; ++i)
+		if(!test_create_string_from_uint(POSITIVE))
+			return false;
+	return true;
 }
 
 bool test_create_string_from_uint_2(void) {
-	unbounded_int u = string2unbounded_int("-75486");
-	char *s = unbounded_int2string(u);
-	bool result = strcmp(s, "-75486") == 0;
-	free_unbounded_int(&u);
-	free(s);
-	return result;
+	for(int i = 0; i < 100; ++i)
+		if(!test_create_string_from_uint(NEGATIVE))
+			return false;
+	return true;
 }
 
-bool test_uint_cmp_with_uint(void) {
-	unbounded_int u = string2unbounded_int("8908");
-	unbounded_int v = string2unbounded_int("3678");
+#define cmp(a, b) (((a) < (b)) ? -1 : ((a) > (b)))
 
-	bool result = unbounded_int_cmp_unbounded_int(u, v) == 1;
-	if(result && unbounded_int_cmp_unbounded_int(v, u) != -1) result = false;
+static bool test_uint_cmp_with_uint(long long a, long long b) {
+	unbounded_int u = ll2unbounded_int(a);
+	unbounded_int v = ll2unbounded_int(b);
+
+	bool result = unbounded_int_cmp_unbounded_int(u, v) == cmp(a, b);
+	if(result && unbounded_int_cmp_unbounded_int(v, u) != cmp(b, a)) result = false;
 	if(result && unbounded_int_cmp_unbounded_int(u, u) != 0) result = false;
 	if(result && unbounded_int_cmp_unbounded_int(v, v) != 0) result = false;
 
 	free_unbounded_int(&u);
 	free_unbounded_int(&v);
 	return result;
+}
+
+bool test_uint_cmp_with_uint_1(void) {
+	for(int i = 0; i < 100; ++i)
+		if(!test_uint_cmp_with_uint(POSITIVE, POSITIVE))
+			return false;
+	return true;
 }
 
 bool test_uint_cmp_with_uint_2(void) {
-	unbounded_int u = string2unbounded_int("-8908");
-	unbounded_int v = string2unbounded_int("-3678");
-
-	bool result = unbounded_int_cmp_unbounded_int(u, v) == -1;
-	if(result && unbounded_int_cmp_unbounded_int(v, u) != 1) result = false;
-	if(result && unbounded_int_cmp_unbounded_int(u, u) != 0) result = false;
-	if(result && unbounded_int_cmp_unbounded_int(v, v) != 0) result = false;
-
-	free_unbounded_int(&u);
-	free_unbounded_int(&v);
-	return result;
+	for(int i = 0; i < 100; ++i)
+		if(!test_uint_cmp_with_uint(NEGATIVE, NEGATIVE))
+			return false;
+	return true;
 }
 
 bool test_uint_cmp_with_uint_3(void) {
-	unbounded_int u = string2unbounded_int("-8908");
-	unbounded_int v = string2unbounded_int("3678");
+	for(int i = 0; i < 100; ++i)
+		if(!test_uint_cmp_with_uint(POSITIVE, NEGATIVE))
+			return false;
+	return true;
+}
 
-	bool result = unbounded_int_cmp_unbounded_int(u, v) == -1;
-	if(result && unbounded_int_cmp_unbounded_int(v, u) != 1) result = false;
-	if(result && unbounded_int_cmp_unbounded_int(u, u) != 0) result = false;
-	if(result && unbounded_int_cmp_unbounded_int(v, v) != 0) result = false;
-
+static bool test_uint_cmp_with_ll(long long a, long long b) {
+	unbounded_int u = ll2unbounded_int(a);
+	bool result = unbounded_int_cmp_ll(u, b) == cmp(a, b);
 	free_unbounded_int(&u);
-	free_unbounded_int(&v);
 	return result;
 }
 
-bool test_uint_cmp_with_ll(void) {
-	unbounded_int u = string2unbounded_int("6404");
-	bool result = unbounded_int_cmp_ll(u, 3740) == 1;
-	if(result && unbounded_int_cmp_ll(u, -3740) != 1) result = false;
-	if(result && unbounded_int_cmp_ll(u, 73512) != -1) result = false;
-	if(result && unbounded_int_cmp_ll(u, 6404) != 0) result = false;
-	free_unbounded_int(&u);
-	return result;
+bool test_uint_cmp_with_ll_1(void) {
+	for(int i = 0; i < 100; ++i)
+		if(!test_uint_cmp_with_ll(POSITIVE, POSITIVE))
+			return false;
+	return true;
 }
 
 bool test_uint_cmp_with_ll_2(void) {
-	unbounded_int u = string2unbounded_int("-3832");
-	bool result = unbounded_int_cmp_ll(u, 657) == -1;
-	if(result && unbounded_int_cmp_ll(u, -370) != -1) result = false;
-	if(result && unbounded_int_cmp_ll(u, -73512) != 1) result = false;
-	if(result && unbounded_int_cmp_ll(u, -3832) != 0) result = false;
+	for(int i = 0; i < 100; ++i)
+		if(!test_uint_cmp_with_ll(POSITIVE, NEGATIVE))
+			return false;
+	return true;
+}
+
+bool test_uint_cmp_with_ll_3(void) {
+	for(int i = 0; i < 100; ++i)
+		if(!test_uint_cmp_with_ll(NEGATIVE, POSITIVE))
+			return false;
+	return true;
+}
+
+static bool test_uint_sum(long long a, long long b) {
+	unbounded_int u = ll2unbounded_int(a);
+	unbounded_int v = ll2unbounded_int(b);
+	unbounded_int w = ll2unbounded_int(a + b);
+
+	unbounded_int res1 = unbounded_int_somme(u, v);
+	unbounded_int res2 = unbounded_int_somme(u, v);
+	bool result = unbounded_int_cmp_unbounded_int(res1, w) == 0 && unbounded_int_cmp_unbounded_int(res2, w) == 0;
+
+	free_unbounded_int(&res1);
+	free_unbounded_int(&res2);
+	free_unbounded_int(&w);
+	free_unbounded_int(&v);
 	free_unbounded_int(&u);
 	return result;
 }
 
-bool test_uint_sum(void) {
-	unbounded_int u = string2unbounded_int("478");
-	unbounded_int v = ZERO;
-
-	unbounded_int sum = unbounded_int_somme(u, v);
-	bool result = unbounded_int_cmp_ll(sum, 478) == 0;
-	if(result) {
-		free_unbounded_int(&sum);
-		sum = unbounded_int_somme(v, u);
-		result = unbounded_int_cmp_ll(sum, 478) == 0;
-	}
-
-	free_unbounded_int(&sum);
-	free_unbounded_int(&u);
-	free_unbounded_int(&v);
-	return result;
+bool test_uint_sum_1(void) {
+	return test_uint_sum(POSITIVE, 0);
 }
 
 bool test_uint_sum_2(void) {
-	unbounded_int u = ZERO;
-	unbounded_int v = ZERO;
-
-	unbounded_int sum = unbounded_int_somme(u, v);
-	bool result = unbounded_int_cmp_ll(sum, 0) == 0;
-	if(result) {
-		free_unbounded_int(&sum);
-		sum = unbounded_int_somme(v, u);
-		result = unbounded_int_cmp_ll(sum, 0) == 0;
-	}
-
-	free_unbounded_int(&sum);
-	free_unbounded_int(&u);
-	free_unbounded_int(&v);
-	return result;
+	return test_uint_sum(NEGATIVE, 0);
 }
 
 bool test_uint_sum_3(void) {
-	unbounded_int u = string2unbounded_int("-478");
-	unbounded_int v = ZERO;
-
-	unbounded_int sum = unbounded_int_somme(u, v);
-	bool result = unbounded_int_cmp_ll(sum, -478) == 0;
-	if(result) {
-		free_unbounded_int(&sum);
-		sum = unbounded_int_somme(v, u);
-		result = unbounded_int_cmp_ll(sum, -478) == 0;
-	}
-
-	free_unbounded_int(&sum);
-	free_unbounded_int(&u);
-	free_unbounded_int(&v);
-	return result;
+	for(int i = 0; i < 100; ++i)
+		if(!test_uint_sum(POSITIVE, POSITIVE))
+			return false;
+	return true;
 }
 
 bool test_uint_sum_4(void) {
-	unbounded_int u = string2unbounded_int("-478");
-	unbounded_int v = string2unbounded_int("-6632");
-
-	unbounded_int sum = unbounded_int_somme(u, v);
-	bool result = unbounded_int_cmp_ll(sum, -7110) == 0;
-	if(result) {
-		free_unbounded_int(&sum);
-		sum = unbounded_int_somme(v, u);
-		result = unbounded_int_cmp_ll(sum, -7110) == 0;
-	}
-
-	free_unbounded_int(&sum);
-	free_unbounded_int(&u);
-	free_unbounded_int(&v);
-	return result;
+	for(int i = 0; i < 100; ++i)
+		if(!test_uint_sum(NEGATIVE, POSITIVE))
+			return false;
+	return true;
 }
 
 bool test_uint_sum_5(void) {
-	unbounded_int u = string2unbounded_int("478");
-	unbounded_int v = string2unbounded_int("-6632");
+	for(int i = 0; i < 100; ++i)
+		if(!test_uint_sum(NEGATIVE, NEGATIVE))
+			return false;
+	return true;
+}
 
-	unbounded_int sum = unbounded_int_somme(u, v);
-	bool result = unbounded_int_cmp_ll(sum, -6154) == 0;
-	if(result) {
-		free_unbounded_int(&sum);
-		sum = unbounded_int_somme(v, u);
-		result = unbounded_int_cmp_ll(sum, -6154) == 0;
-	}
+static bool test_uint_produit(long long a, long long b) {
+	unbounded_int u = ll2unbounded_int(a);
+	unbounded_int v = ll2unbounded_int(b);
+	unbounded_int w = ll2unbounded_int(a * b);
 
-	free_unbounded_int(&sum);
-	free_unbounded_int(&u);
+	unbounded_int res1 = unbounded_int_produit(u, v);
+	unbounded_int res2 = unbounded_int_produit(v, u);
+	bool result = unbounded_int_cmp_unbounded_int(res1, w) == 0 && unbounded_int_cmp_unbounded_int(res2, w) == 0;
+
+	free_unbounded_int(&res1);
+	free_unbounded_int(&res2);
+	free_unbounded_int(&w);
 	free_unbounded_int(&v);
+	free_unbounded_int(&u);
 	return result;
 }
 
-bool test_uint_produit(void) {
-	unbounded_int u = string2unbounded_int("478");
-	unbounded_int v = string2unbounded_int("6632");
-
-	unbounded_int prod = unbounded_int_produit(u, v);
-	bool result = unbounded_int_cmp_ll(prod, 3170096) == 0;
-	if(result) {
-		free_unbounded_int(&prod);
-		prod = unbounded_int_produit(v, u);
-		result = unbounded_int_cmp_ll(prod, 3170096) == 0;
-	}
-
-	free_unbounded_int(&prod);
-	free_unbounded_int(&u);
-	free_unbounded_int(&v);
-	return result;
+bool test_uint_produit_1(void) {
+	return test_uint_produit(POSITIVE, 0) && test_uint_produit(NEGATIVE, 0);
 }
 
 bool test_uint_produit_2(void) {
-	unbounded_int u = string2unbounded_int("478");
-	unbounded_int v = ZERO;
-
-	unbounded_int prod = unbounded_int_produit(u, v);
-	bool result = unbounded_int_cmp_unbounded_int(prod, v) == 0;
-	if(result) {
-		free_unbounded_int(&prod);
-		prod = unbounded_int_produit(v, u);
-		result = unbounded_int_cmp_unbounded_int(prod, v) == 0;
-	}
-
-	free_unbounded_int(&prod);
-	free_unbounded_int(&u);
-	free_unbounded_int(&v);
-	return result;
+	return test_uint_produit(POSITIVE, 1) && test_uint_produit(NEGATIVE, 1);
 }
 
 bool test_uint_produit_3(void) {
-	unbounded_int u = string2unbounded_int("-478");
-	unbounded_int v = ZERO;
-
-	unbounded_int prod = unbounded_int_produit(u, v);
-	bool result = unbounded_int_cmp_unbounded_int(prod, v) == 0;
-	if(result) {
-		free_unbounded_int(&prod);
-		prod = unbounded_int_produit(v, u);
-		result = unbounded_int_cmp_unbounded_int(prod, v) == 0;
-	}
-
-	free_unbounded_int(&prod);
-	free_unbounded_int(&u);
-	free_unbounded_int(&v);
-	return result;
+	for(int i = 0; i < 100; ++i)
+		if(!test_uint_produit(POSITIVE, POSITIVE))
+			return false;
+	return true;
 }
 
 bool test_uint_produit_4(void) {
-	unbounded_int u = string2unbounded_int("-478");
-	unbounded_int v = string2unbounded_int("-6632");
+	for(int i = 0; i < 100; ++i)
+		if(!test_uint_produit(NEGATIVE, NEGATIVE))
+			return false;
+	return true;
+}
 
-	unbounded_int prod = unbounded_int_produit(u, v);
-	bool result = unbounded_int_cmp_ll(prod, 3170096) == 0;
-	if(result) {
-		free_unbounded_int(&prod);
-		prod = unbounded_int_produit(v, u);
-		result = unbounded_int_cmp_ll(prod, 3170096) == 0;
-	}
+bool test_uint_produit_5(void) {
+	for(int i = 0; i < 100; ++i)
+		if(!test_uint_produit(NEGATIVE, POSITIVE))
+			return false;
+	return true;
+}
 
-	free_unbounded_int(&prod);
-	free_unbounded_int(&u);
+static long long powll(long long a, long long b) {
+	long long res = 1;
+	while(b-- > 0) res *= a;
+	return res;
+}
+
+static bool test_uint_pow(long long a, long long b) {
+	unbounded_int u = ll2unbounded_int(a);
+	unbounded_int v = ll2unbounded_int(b);
+
+	unbounded_int res1 = unbounded_int_pow(u, v);
+	bool result;
+	if(b >= 0) {
+		unbounded_int w = ll2unbounded_int(powll(a, b));
+		result = unbounded_int_cmp_unbounded_int(res1, w) == 0;
+		free_unbounded_int(&w);
+	} else result = isNaN(res1);
+
+	free_unbounded_int(&res1);
 	free_unbounded_int(&v);
+	free_unbounded_int(&u);
 	return result;
 }
 
-bool test_uint_pow(void) {
-	unbounded_int u = string2unbounded_int("9");
-	unbounded_int v = string2unbounded_int("5");
-
-	unbounded_int pow = unbounded_int_pow(u, v);
-	bool result = unbounded_int_cmp_ll(pow, 59049) == 0;
-	if(result) {
-		free_unbounded_int(&pow);
-		pow = unbounded_int_pow(v, u);
-		result = unbounded_int_cmp_ll(pow, 1953125) == 0;
-	}
-
-	free_unbounded_int(&pow);
-	free_unbounded_int(&u);
-	free_unbounded_int(&v);
-	return result;
+bool test_uint_pow_1(void) {
+	return test_uint_pow(random_ll(0, 1000), 0) && test_uint_pow(0, random_ll(0, 1000));
 }
 
 bool test_uint_pow_2(void) {
-	unbounded_int u = string2unbounded_int("0");
-	unbounded_int v = string2unbounded_int("243");
-
-	unbounded_int pow = unbounded_int_pow(u, v);
-	bool result = unbounded_int_cmp_ll(pow, 0) == 0;
-	if(result) {
-		free_unbounded_int(&pow);
-		pow = unbounded_int_pow(v, u);
-		result = unbounded_int_cmp_ll(pow, 1) == 0;
-	}
-
-	free_unbounded_int(&pow);
-	free_unbounded_int(&u);
-	free_unbounded_int(&v);
-	return result;
+	for(int i = 1; i < 16; ++i)
+		for(int j = 1; j < 16; ++j)
+			if(!test_uint_pow(i, j))
+				return false;
+	return true;
 }
 
 bool test_uint_pow_3(void) {
-	unbounded_int u = string2unbounded_int("-9");
-	unbounded_int v = string2unbounded_int("-3");
-
-	unbounded_int pow = unbounded_int_pow(u, v);
-	bool result = isNaN(pow);
-	if(result) {
-		free_unbounded_int(&pow);
-		pow = unbounded_int_pow(v, u);
-		result = isNaN(pow);
-	}
-
-	free_unbounded_int(&pow);
-	free_unbounded_int(&u);
-	free_unbounded_int(&v);
-	return result;
+	for(int i = 0; i < 100; ++i)
+		if(!test_uint_pow(POSITIVE, NEGATIVE))
+			return false;
+	return true;
 }
 
 bool test_uint_pow_4(void) {
-	unbounded_int u = string2unbounded_int("-9");
-	unbounded_int v = string2unbounded_int("3");
+	for(int i = 0; i < 100; ++i)
+		if(!test_uint_pow(NEGATIVE, NEGATIVE))
+			return false;
+	return true;
+}
 
-	unbounded_int pow = unbounded_int_pow(u, v);
-	bool result = unbounded_int_cmp_ll(pow, -729) == 0;
-	if(result) {
-		free_unbounded_int(&pow);
-		pow = unbounded_int_pow(v, u);
-		result = isNaN(pow);
-	}
-
-	free_unbounded_int(&pow);
-	free_unbounded_int(&u);
-	free_unbounded_int(&v);
-	return result;
+bool test_uint_pow_5(void) {
+	for(int i = -15; i < 0; ++i)
+		for(int j = 1; j < 16; ++j)
+			if(!test_uint_pow(i, j))
+				return false;
+	return true;
 }
--- test/test_unbounded_int.h
+++ test/test_unbounded_int.h
@@ -5,7 +5,7 @@
 
 // Test the creation of an unbounded int from a string
 
-extern bool test_create_uint_from_string(void);
+extern bool test_create_uint_from_string_1(void);
 
 extern bool test_create_uint_from_string_2(void);
 
@@ -15,31 +15,33 @@ extern bool test_create_uint_from_string_with_invalid_string_2(void);
 
 // Test the creation of an unbounded int from a long long int
 
-extern bool test_create_uint_from_ll(void);
+extern bool test_create_uint_from_ll_1(void);
 
 extern bool test_create_uint_from_ll_2(void);
 
 // Test the creation of a string from an unbounded int
 
-extern bool test_create_string_from_uint(void);
+extern bool test_create_string_from_uint_1(void);
 
 extern bool test_create_string_from_uint_2(void);
 
 // Test the comparison of an unbounded int with another unbounded int or a long long int
 
-extern bool test_uint_cmp_with_uint(void);
+extern bool test_uint_cmp_with_uint_1(void);
 
 extern bool test_uint_cmp_with_uint_2(void);
 
 extern bool test_uint_cmp_with_uint_3(void);
 
-extern bool test_uint_cmp_with_ll(void);
+extern bool test_uint_cmp_with_ll_1(void);
 
 extern bool test_uint_cmp_with_ll_2(void);
 
+extern bool test_uint_cmp_with_ll_3(void);
+
 // Test the sum of two unbounded int
 
-extern bool test_uint_sum(void);
+extern bool test_uint_sum_1(void);
 
 extern bool test_uint_sum_2(void);
 
@@ -51,7 +53,7 @@ extern bool test_uint_sum_5(void);
 
 // Test the product of two unbounded int
 
-extern bool test_uint_produit(void);
+extern bool test_uint_produit_1(void);
 
 extern bool test_uint_produit_2(void);
 
@@ -59,14 +61,18 @@ extern bool test_uint_produit_3(void);
 
 extern bool test_uint_produit_4(void);
 
+extern bool test_uint_produit_5(void);
+
 // Test the power of an unbounded int by another unbounded int
 
-extern bool test_uint_pow(void);
+extern bool test_uint_pow_1(void);
 
 extern bool test_uint_pow_2(void);
 
 extern bool test_uint_pow_3(void);
 
 extern bool test_uint_pow_4(void);
+
+extern bool test_uint_pow_5(void);
 
 #endif //PROJET_TEST_UNBOUNDED_INT_H
