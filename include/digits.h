4857c3400aac9558ffe4e2a2bbd2b1037d74c206
Hocine <ocineh@hotmail.com>
2022-11-19 11:12:10 +0100

    Add digits struct and functions to create and delete them

---
+++ include/digits.h
@@ -0,0 +1,10 @@
+#ifndef DIGITS_H
+#define DIGITS_H
+
+typedef struct digits digits;
+
+digits* new_digits(void);
+
+void free_digits(digits* d);
+
+#endif //DIGITS_H
---
+++ src/digits.c
@@ -0,0 +1,32 @@
+#include <malloc.h>
+#include "digits.h"
+
+#define DEFAULT_CAPACITY 10
+
+struct digits {
+	char* digits;
+	size_t begin, end;
+	size_t length;
+	size_t capacity;
+};
+
+digits* new_digits(void) {
+	digits* d = malloc(sizeof(digits));
+	if(d == NULL) return NULL;
+
+	d->digits = calloc(DEFAULT_CAPACITY, sizeof(char));
+	if(d->digits == NULL) {
+		free(d);
+		return NULL;
+	}
+
+	d->begin = d->end = 0;
+	d->capacity = DEFAULT_CAPACITY;
+	return d;
+}
+
+void free_digits(digits* d) {
+	if(d == NULL) return;
+	free(d->digits);
+	free(d);
+}
